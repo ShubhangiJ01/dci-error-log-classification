ef5eb9fe330bd6376c78723811841d02cf168e0c
Shubhangi Jain <shujain@shujain.bos.csb>
2020-07-13 17:22:15 -0400

    Adding logging, main function

---
+++ classifier/main.py
@@ -0,0 +1,52 @@
+
+import argparse
+import sys
+import logging
+import traceback
+
+from error_classification_rhel import data_load
+from error_classification_rhel import feature_generation
+from error_classification_rhel import classifier_rules
+from error_classification_rhel import classification_storage
+
+
+LOG = logging.getLogger(__name__)
+#LOG.setLevel(logging.DEBUG)
+logging.getLogger().setLevel(logging.INFO)
+
+def main():
+    parser = argparse.ArgumentParser(description='DCI error log classification')
+    parser.add_argument('product',type=str, help='Product value')
+    args = parser.parse_args()
+    
+    if(args.product == "rhel"):
+        try:
+            logging.info('Loading data for RHEL')
+            data = data_load()
+        except Exception:
+            LOG.error(traceback.format_exc())
+            sys.exit(1)
+
+        try:
+            logging.info('Entering feature generator')
+            final_data = feature_generation(data)
+        except Exception:
+            LOG.error(traceback.format_exc())
+            sys.exit(1)
+
+        try:    
+            logging.info('Calling classifier')
+            classified_data = classifier_rules(final_data)
+        except Exception:
+            LOG.error(traceback.format_exc())
+            sys.exit(1)
+
+        try:
+            logging.info('Writing labeled data in a csv file')
+            classification_storage(classified_data)
+        except Exception:
+            LOG.error(traceback.format_exc())
+            sys.exit(1)
+
+if __name__ == '__main__':
+    main()
