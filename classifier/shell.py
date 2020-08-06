b1f123ba3856590300cb5a5cf928ce3f09b8cb0f
Shubhangi Jain <shujain@shujain.bos.csb>
2020-08-06 16:41:25 -0400

    New Code

---
+++ classifier/shell.py
@@ -0,0 +1,18 @@
+import argparse
+import sys
+import logging
+import traceback
+import os
+from classifier.shell_commands.runner import run
+from classifier.settings import parse_argument
+
+LOG = logging.getLogger(__name__)
+
+def main():
+    args = parse_argument()    
+    try:
+        run(args)
+    except Exception:
+        LOG.error(traceback.format_exc())
+        sys.exit(1)
+
